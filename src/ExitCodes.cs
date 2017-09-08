045df219e44366b798db76b739d3641069686d53
Chase Florell <chase.florell@gmail.com>
2017-09-08 15:58:41 -0700

    [CF] Added appropriate exit codes

---
+++ src/ExitCodes.cs
@@ -0,0 +1,15 @@
+﻿namespace HockeyApp
+{
+    public static class ExitCodes
+    {
+        /// <summary>
+        /// Used when command fails to execute successfully
+        /// </summary>
+        public static int CommandFailed => 1;
+
+        /// <summary>
+        /// Used when the user specified an invalid command
+        /// </summary>
+        public static int InvalidCommand => 2;
+    }
+}
